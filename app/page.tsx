d15dd3d61f0b04b5ad23d391a02675d95945b548
Nipun <nipunrekhi3@gmail.com>
2024-03-03 15:16:14 +0530

    Added Floating Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,23 @@ import Contact from "@/components/component/contact";
 import Main from "@/components/component/main";
 import Projects from "@/components/component/projects";
 import Skills from "@/components/component/skills";
+import { useParams } from "next/navigation";
 
 export default function Home() {
+  
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 ">
+        <section className="w-full py-12 md:py-24 lg:py-32 " id="home">
           <Main />
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+        <section className="w-full py-12 md:py-24 lg:py-32 border-t" id="projects">
           <Projects />
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+        <section className="w-full py-12 md:py-24 lg:py-32 border-t" id="skills">
           <Skills />
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+        <section className="w-full py-12 md:py-24 lg:py-32 border-t" id="contact">
           <Contact />
         </section>
       </main>
--- components/component/header.tsx
+++ components/component/header.tsx
@@ -1,33 +1,62 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { ModeToggle } from "../ui/mode-toggler";
-
+import { Button } from "../ui/button";
+import { FloatingNav } from "../ui/floating-navbar";
+const navItems = [
+  {
+    name: "Home",
+    id: "home",
+  },
+  {
+    name: "Projects",
+    id: "projects",
+  },
+  {
+    name: "Skills",
+    id: "skills",
+  },
+  {
+    name: "Contact",
+    id: "contact",
+  },
+];
 const Header = () => {
+  const scrollToPage = (id: string) => {
+    const element = document.getElementById(id) as HTMLElement;
+    if (element)
+      element.scrollIntoView({ behavior: "smooth", inline: "start" });
+  };
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center relative ">
+    <header className="px-4 lg:px-6 h-14 flex items-center relative  ">
       <Link className="flex items-center justify-center" href="#">
         <MountainIcon className="h-6 w-6" />
         <span className="sr-only">Acme Inc</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6 relative">
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
-        >
-          Projects
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
-        >
-          About
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
-        >
-          Contact
-        </Link>
+        <FloatingNav>
+          {navItems.map(({ name, id }, idx) => (
+            <Button
+              key={name + "" + idx}
+              variant={"link"}
+              onClick={(e) => scrollToPage(id)}
+              className="text-sm font-medium hover:underline underline-offset-4"
+            >
+              {name}
+            </Button>
+          ))}
+        </FloatingNav>
+        {navItems.map(({ name, id }, idx) => (
+          <Button
+            key={name + "" + idx}
+            variant={"link"}
+            onClick={(e) => scrollToPage(id)}
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            {name}
+          </Button>
+        ))}
         <div className="text-sm font-medium hover:underline underline-offset-4">
           <ModeToggle />
         </div>
